15b3350965f780bdd0bbc31aa7355156ba79d138
cord_NiA <c0b231461d@edu.teu.ac.jp>
2024-06-25 14:47:30 +0900

    gameoverの処理変更

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -162,9 +162,14 @@ def main():
         if bomb != None:
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-                bird.change_img(8, screen)
-                pg.display.update()
-                time.sleep(1)
+                # bird.change_img(8, screen)
+                # pg.display.update()
+                # time.sleep(1)
+                fonto = pg.font.Font(None, 80) 
+                txt = fonto.render("Game Over", True, (255, 0, 0)) 
+                screen.blit(txt, [WIDTH/2-150, HEIGHT/2]) 
+                pg.display.update() 
+                time.sleep(5)
                 return
         else:
             bird.change_img(6, screen)
